6a14a7cf8a20ed4999ed41f65ec6e1e4591e402c
ast8705 <andtoskas1104@gmail.com>
2022-08-26 13:46:19 -0400

    checked win cases

--- src/game.py
+++ src/game.py
@@ -11,6 +11,25 @@ def printBoard(board):
     for row in range(len(board)):
         print(board[row])
 
+def win(board):
+    if(board[0][0] == board [0][1] == board[0][2]):
+        return board[0][0] 
+    if(board[1][0] == board [1][1] == board[1][2]):
+        return board[1][0]
+    if(board[2][0] == board [2][1] == board[2][2]):
+        return board[2][0]
+    if(board[0][0] == board [1][0] == board[2][0]):
+        return board[0][0]
+    if(board[0][1] == board [1][1] == board[2][1]):
+        return board[0][1]
+    if(board[0][2] == board [1][2] == board[2][2]):
+        return board[0][2]
+    if(board[0][0] == board [1][1] == board[2][2]):
+        return board[0][0]
+    if(board[2][0] == board [1][1] == board[0][2]):
+        return board[2][0]
+    return 0
+
 while not isOver:
     printBoard(board)
     if(playerTurn):
@@ -23,6 +42,12 @@ while not isOver:
         board[row][col] = 2
     
     playerTurn = not playerTurn
-    gameCount = gameCount + 1
-    if(gameCount >= 9):
+    x = win(board)
+    gameCount += 1
+    
+    if(x > 0 ):
         isOver = True
+        print("Winner is: " + str(x))
+    elif(gameCount == 8):
+        isOver = True
+        print("Tie")
