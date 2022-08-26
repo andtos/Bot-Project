7fddcb8da5868cd19ddfeccd5d77507abbf73e65
gameboy2003 <ethan.pascual@gmail.com>
2022-08-26 13:24:15 -0400

    HELLO

--- src/game.py
+++ src/game.py
@@ -2,6 +2,21 @@ from array import *;
 board = [[0,0,0], [0,0,0], [0,0,0]]
 print( board)
 isOver= False
+playerTurn = True
+row = 0
+column = 0
+gameCount = 0
 while not isOver:
-    print("penis")
-    isOver = True
+    if(playerTurn):
+        row = input('Player 1: Enter chosen row')
+        col = input('Player 1: Enter chosen column')
+        board[row][col] = 1
+    else:
+        row = input('Player 2: Enter chosen row')
+        col = input('Player 2: Enter chosen column')
+        board[row][col] = 2
+    
+    playerTurn = not playerTurn
+    gameCount = gameCount + 1
+    if(gameCount >= 9):
+        isOver = True
