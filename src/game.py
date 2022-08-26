dd43cb1192d58eee892983071612931b4a484b4e
BabyDinos <108362808+BabyDinos@users.noreply.github.com>
2022-08-26 11:25:56 -0700

    win2 now works

--- src/game.py
+++ src/game.py
@@ -1,6 +1,7 @@
-from array import *;
-board = [[0,0,0], [0,0,0], [0,0,0]]
-print( board)
+#from array import *;
+#board = [[0,0,0], [0,0,0], [0,0,0]]
+import numpy as np
+board = np.zeros(shape = (3,3))
 isOver= False
 playerTurn = True
 row = 0
@@ -30,6 +31,20 @@ def win(board):
         return board[2][0]
     return 0
 
+def win2(board):
+    for row in range(len(board)):
+        if all(board[row]) == 1 or all(board[row]) == -1:
+            return board[row][0]
+    for col in range(len(board[0])):
+        if all(board[:,[col]]) == 1 or all(board[:,[col]]) == -1:
+            return board[0][col]        
+    if all(np.diag(board)) == 1 or all(np.diag(board)) == -1:
+        return board[1][1]
+    if sum(np.diag(board[::-1])[::-1]) == -3 or sum(np.diag(board[::-1])[::-1]) == 3:
+        return board[1][1]
+    return 0
+
+
 while not isOver:
     printBoard(board)
     if(playerTurn):
@@ -45,7 +60,7 @@ while not isOver:
         row = int(input('Player 2: Enter chosen row: '))
         col = int(input('Player 2: Enter chosen column: '))
         if(board[row][col] == 0 and row > -1 and row < 3 and col > -1 and col < 3):
-            board[row][col] = 2
+            board[row][col] = -1
         else:
             print('Invalid Entry, please try again.')
             continue
